89021f182dfdf41f4ce4eb214fedf6dcbb9c9b25
jooyeal <34053954+jooyeal@users.noreply.github.com>
2023-04-23 15:38:34 +0900

    ADD: Chakra-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { trpc } from "../utils/trpc";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default trpc.withTRPC(function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 });
